ec6dee9de40dce1aefa596463dfa5144be174fc6
Christian <ChristianLMedlin@gmail.com>
2019-03-07 11:58:36 -0500

    Completed 1-5

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-]
-
-urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
